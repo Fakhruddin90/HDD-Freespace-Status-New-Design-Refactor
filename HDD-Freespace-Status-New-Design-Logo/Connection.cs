5b261beeb27e7c54d7d384ee7c240f280fed6eb7
Fakhruddin90 <Fakhruddin90.com>
2020-11-11 18:00:43 +0800

    add logo, database api and streamwriter function

---
+++ HDD-Freespace-Status-New-Design-Logo/Connection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace HDD_Freespace_Status_Design_1
+{
+    public class Connection
+    {
+        public static void SQLConnection()
+        {
+            string server = "localhost";
+            string user = "root";
+            string database = "database";
+            int port = 3306;
+            string password = "";
+
+            string connStr = $"server={server};user={user};database={database};port={port};password={password}";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                MessageBox.Show("Connecting to MySQL...");
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            conn.Close();
+            MessageBox.Show("Disconnected from MySQL");
+        }
+    }
+}
---
+++ HDD-Freespace-Status-New-Design-Logo/Logs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HDD_Freespace_Status_Design_1
+{
+    public class Logs
+    {
+        public static void CSVLogs(string text)
+        {
+            //string text = "A class is the most powerful data type in C#. Like a structure, " +
+            //    "a class defines the data and behavior of the data type. ";
+            // WriteAllText creates a file, writes the specified string to the file,
+            // and then closes the file. You do NOT need to call Flush() or Close().
+            string filename = Application.StartupPath + $"\\CSVLogs.txt";
+            System.IO.File.WriteAllText(filename, text);
+        }
+    }
+}
